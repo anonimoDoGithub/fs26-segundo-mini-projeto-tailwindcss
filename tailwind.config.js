1f294f156b004bf2b9d7cc4ed753f11ebd82422e
anonimoDoGithub <xxexpgamerxx@gmail.com>
2023-10-06 11:22:00 -0300

    primeiro ponto

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "bg-ainsten": "#3f3456"
+      }
+    },
+  },
+  plugins: [],
+}
+
